7c7835fa256cc85216d8badd7ee2f1557e1675a6
Jared Button <114765054+jaredb347@users.noreply.github.com>
2023-07-05 15:45:11 -0400

    Commit2

--- mongo-dao.js
+++ mongo-dao.js
@@ -2,33 +2,77 @@ const mongodb = require("mongodb"); // mongo client library
 const { MongoClient } = require("mongodb");
 const url = "mongodb://localhost:27017";
 const dbName = "swapi";
-const collectionName = "planets";
+//let collectionName = "planets";
 let collection;
+let dbPool;
 async function startup() {
   let client = new MongoClient(url);
   await client.connect();
-  var db = client.db(dbName);
-  collection = db.collection(collectionName);
+  dbPool = client.db(dbName);
+  //collection = db.collection(collectionName);
 }
 
 startup();
 //find all planets
 module.exports.findAllPlanets = function (callback) {
+  collection = dbPool.collection("planets");
   let dataPromise = collection.find({}).toArray();
   dataPromise.then((planets) => callback(planets));
 };
 // find a single planet
 module.exports.findPlanet = function (ids, callback) {
+  collection = dbPool.collection("planets");
   let dataPromise = collection.findOne({ id: +ids });
   dataPromise.then((planet) => callback(planet));
 };
 //find all characters
 module.exports.findAllCharacters = function (callback) {
+  collection = dbPool.collection("characters");
   let dataPromise = collection.find({}).toArray();
   dataPromise.then((characters) => callback(characters));
 };
 //find one character
 module.exports.findCharacter = function (ids, callback) {
+  collection = dbPool.collection("characters");
   let dataPromise = collection.findOne({ id: +ids });
   dataPromise.then((character) => callback(character));
 };
+//find all films
+module.exports.findAllFilms = function (callback) {
+  collection = dbPool.collection("films");
+  let dataPromise = collection.find({}).toArray();
+  dataPromise.then((films) => callback(films));
+};
+//find one film
+module.exports.findFilm = function (ids, callback) {
+  collection = dbPool.collection("films");
+  let dataPromise = collection.findOne({ id: +ids });
+  dataPromise.then((film) => callback(film));
+};
+//find all characters from a specific film
+module.exports.findCharactersByFilm = function (ids, callback) {
+  collection = dbPool.collection("characters");
+  let dataPromise = collection.findOne({ id: +ids });
+  dataPromise.then((character) => callback(character));
+};
+
+// module.exports.findCharactersByFilm = async function (id, callback) {
+//   collection = dbPool.collection("films_characters");
+//   let dataPromise = collection
+//     .find({ film_id: +id })
+//     .toArray(async (err, matches) => {
+//       let characters = [];
+//       for (let match of matches) {
+//         let character_id = match.character_id;
+//         console.log(character_id);
+//         collection = dbPool.collection("characters");
+//         let data = await collection.find({ id: +character_id }).toArray();
+//         characters.push({ id: character_id, name: data[0].name });
+//       }
+//       if (!err) {
+//         dataPromise.then((characters) => callback(characters));
+//       } else {
+//         callback("failed to find characters", undefined);
+//       }
+//     });
+// };
--- server.js
+++ server.js
@@ -6,10 +6,7 @@ const port = 3000;
 console.log("server starting on port: " + port);
 app.listen(port);
 
-/*app.get("/api/planets", function (req, res) {
-  res.send({ planet: "test-planet" });
-});*/
-
+//all planets
 app.get("/api/planets", (req, res) => {
   dao.findAllPlanets((planets) => {
     if (!planets) {
@@ -19,13 +16,63 @@ app.get("/api/planets", (req, res) => {
     }
   });
 });
-
+//oner planet
 app.get("/api/planets/:id", (req, res) => {
   dao.findPlanet(req.params.id, (planet) => {
     if (!planet) {
       res.status(404).end();
     } else {
       res.send(planet);
+    }
+  });
+});
+//find all the people
+app.get("/api/characters", (req, res) => {
+  dao.findAllCharacters((characters) => {
+    if (!characters) {
+      res.status(404).end();
+    } else {
+      res.send(characters);
+    }
+  });
+});
+//find one person
+app.get("/api/characters/:id", (req, res) => {
+  dao.findCharacter(req.params.id, (character) => {
+    if (!character) {
+      res.status(404).end();
+    } else {
+      res.send(character);
+    }
+  });
+});
+//find all the films
+app.get("/api/films", (req, res) => {
+  dao.findAllFilms((films) => {
+    if (!films) {
+      res.status(404).end();
+    } else {
+      res.send(films);
+    }
+  });
+});
+//find one film
+app.get("/api/films/:id", (req, res) => {
+  dao.findFilm(req.params.id, (film) => {
+    if (!film) {
+      res.status(404).end();
+    } else {
+      res.send(film);
+    }
+  });
+});
+//find all characters from a specific film
+app.get("/api/films/:id/characters", (req, res) => {
+  dao.findCharactersByFilm(req.params.id, (characters) => {
+    if (!characters) {
+      res.status(404).end();
+    } else {
+      res.send(characters);
     }
   });
 });
